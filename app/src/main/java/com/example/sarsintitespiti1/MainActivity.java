9769755588cea8989fec6a60d9060d8976e0b411
aysedeveden <116079495+aysedeveden@users.noreply.github.com>
2023-11-23 12:57:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sarsintitespiti1/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.sarsintitespiti1;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class MainActivity<sensorManager> extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        TextView btn=findViewById(R.id.textViewSignUp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Sensor.class));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/sarsintitespiti1/giris.java
@@ -0,0 +1,35 @@
+package com.example.sarsintitespiti1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+public class giris extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giris);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        CountDownTimer timer = new CountDownTimer(3000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                Intent intent = new Intent(giris.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        timer.start();
+    }
+}
